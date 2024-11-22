4f3c418e3e2ce47e912cadecca74e5d6113ed161
Lambit Dohotia <ilambitexting@gmail.com>
2024-11-22 14:50:32 +0530

    fixed a line of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import TicTacToe from './components/TicTacToe/TicTacToe'
 import TicTacToe2 from './components/TicTacToe/TicTacToe2'
-import TicTacToe3 from './components/TicTacToe/TicTacToe3'
 
 const App = () => {
   return (
